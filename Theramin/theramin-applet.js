3c066ae8dc3cf5a860b8f74f1336684ff18991d9
Preet <preet@hivepoint.com>
2015-06-02 23:28:05 -0700

    more theramin code

--- Theramin/theramin-applet.js
+++ Theramin/theramin-applet.js
@@ -13,40 +13,98 @@ Polymer('theramin-applet', {
 	reset: function() {
 	  for (var i = 0; i < this.$.braid.members.length; i++) {
 	    var member = this.$.braid.members[i];
-	    var sound = {
-	      oscillator: context.createOscillator(),
-	      amp: context.createGain()
-	    };
-	    sound.oscillator.connect(sound.amp);
-      sound.amp.connect(context.destination);
-	    sound.oscillator.type = 'square';
-      sound.oscillator.frequency.value = 0;
-      sound.oscillator.detune.value = 100;
-      sound.oscillator.start(0);
-      sound.amp.gain.value = 0;
+      this.sounds[member.jid] = this.createSound();
 	  }
 	},
 	
-	refresh: function() {
-		for (var i = 0; i < this.$.braid.members.length; i++) {
-			var member = this.$.braid.members[i];
-			this.positions[member.jid] = {
-				x: -100,
-				y: -100
-			};
-			this.letters[member.jid] = member.jid.substring(0, 1).toUpperCase();
-		}		
+	createSound: function() {
+	  var sound = {
+      oscillator: context.createOscillator(),
+      amp: context.createGain()
+    };
+    sound.oscillator.connect(sound.amp);
+    sound.amp.connect(context.destination);
+    sound.oscillator.type = 'square';
+    sound.oscillator.frequency.value = 0;
+    sound.oscillator.detune.value = 100;
+    sound.oscillator.start(0);
+    sound.amp.gain.value = 0;
+    return sound;
+	},
+	
+	updateSound: function(originator, frequency, volume) {
+	  var jid = originator.split("/")[0];
+	  var member = this.$.braid.membersByJid[jid];
+    if (member) {
+      if (!this.sounds[jid]) {
+        this.sounds[jid] = this.createSound();
+      }
+      // console.log("draw " + frequency + " " + volume + " " + jid);
+      this.sounds[jid].oscillator.frequency.value = frequency * this.maxFreq;
+      this.sounds[jid].amp.gain.value = volume * this.maxVol;
+      this.draw(frequency, volume);
+    }
+	},
+	
+	draw: function(f, v) {
+	  if (f && v) {
+  	  var color = 'rgb(120,' + Math.floor(v * 255) + ',' + Math.floor(f * 255) + ')';
+  	  var div = document.createElement("div");
+  	  div.className = "disc";
+  	  div.style.top = v * this.$.canvas.offsetHeight;
+  	  div.style.left = f * this.$.canvas.offsetWidth;
+  	  div.style.backgroundColor = color;
+  	  this.$.canvas.appendChild(div);
+  	  this.async(function() {
+        div.style.opacity = 0;
+        this.async(function() {
+          div.remove();
+        }, null, 800);
+      }, null, 200);
+	  }
 	},
 	
 	mouseOut: function(event) {
-	  this.volume = 0;
-	  this.frequency = 0;
-	  this.updateSound();
+	  this.updateSound(this.$.braid.profile.jid, 0, 0);
 	},
 	
-	updateSound: function() {
-    oscillator.frequency.value = this.frequency;
-    gainNode.gain.value = this.volume;
+	mouseMove: function(event) {
+	  var x = event.offsetX ? event.offsetX : 0;
+	  var y = event.offsetY ? event.offsetY : 0;
+	  var f =  x / this.$.canvas.offsetWidth;
+	  var v = y / this.$.canvas.offsetHeight;
+	  this.broadcast(f, v);
+	  this.updateSound(this.$.braid.profile.jid, f, v);
+	},
+	
+	broadcast: function(f, v) {
+	  if (!this.broadcasting) {
+	    this.broadcasting = true;
+	    this.pendingBroadcast = null;
+	    this.$.braid.broadcastMessage({
+	      frequency: f,
+	      volume: v
+	    }, function() {
+	      this.broadcasting = false;
+	      if (this.pendingBroadcast) {
+	        this.broadcast(this.pendingBroadcast.frequency, this.pendingBroadcast.volume);
+	      }
+	    }.bind(this));
+	  } else {
+	    this.pendingBroadcast = {
+	      frequency: f,
+	      volume: v
+	    };
+	  }
+	},
+	
+	broadcastReceived: function(event) {
+	  var jid = event.detail.originator;
+	  if (jid) {
+	    var f = event.detail.payload.frequency;
+	    var v = event.detail.payload.volume;
+	    this.updateSound(jid, f, v);
+	  }
 	}
 	
 });
