53bf47ef284e298e2c15dc3720e2b799369845e3
Preet <preet@hivepoint.com>
2015-06-05 15:41:28 -0700

    fixed sliders to have init value

--- sliders/sliders-applet.js
+++ sliders/sliders-applet.js
@@ -1,2 +1,13 @@
 Polymer('sliders-applet', {
+  onReady: function() {
+    if (!this.$.braid.properties.red) {
+      this.$.braid.properties.red = 0;
+    }
+    if (!this.$.braid.properties.blue) {
+      this.$.braid.properties.blue = 0;
+    }
+    if (!this.$.braid.properties.green) {
+      this.$.braid.properties.green = 0;
+    }
+  }
 });
